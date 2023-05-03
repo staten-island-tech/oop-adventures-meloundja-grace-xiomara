f0c9db7ee8d31172f5857da4278d91e97a168877
Meloundja <124072293+Meloundja@users.noreply.github.com>
2023-05-03 10:59:05 -0400

    5/3

--- Intro.py
+++ Intro.py
@@ -3,7 +3,8 @@ print ("Welcome dear traverler")
 answer = input("Would you like to embark on this journey? (yes/no)" )
 if answer.lower().strip() == "yes":
     print("Whoopy")
+    print("You have fifty days to make")
     
 else:
     print("Oh well, farrell dear traverler")
-    break
+    
