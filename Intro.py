6e6117ec4f44b9afe5e443ce0996084f741d509b
Meloundja <124072293+Meloundja@users.noreply.github.com>
2023-05-03 10:58:40 -0400

    5/3

--- Intro.py
+++ Intro.py
@@ -1,1 +1,9 @@
-import turtle
+print ("Welcome dear traverler")
+
+answer = input("Would you like to embark on this journey? (yes/no)" )
+if answer.lower().strip() == "yes":
+    print("Whoopy")
+    
+else:
+    print("Oh well, farrell dear traverler")
+    break
