5fc338012714addb1be81588440d8b5dee204ed3
Meloundja <124072293+Meloundja@users.noreply.github.com>
2023-05-04 10:32:05 -0400

    5/4

--- Intro.py
+++ Intro.py
@@ -3,7 +3,13 @@ print ("Welcome dear traverler")
 answer = input("Would you like to embark on this journey? (yes/no)" )
 if answer.lower().strip() == "yes":
     print("Whoopy")
-    print("You have fifty days to make")
+    print("You have fifty days to make to the Kingdom, or your your treasure will forever be lost.")
+    print("Within these fifty days, you'll be tasked with diffrent choices in which will either get you to your destination or you will unfourtnly be eliminated")
+   
+answer = input("are you ready for your first choice?")
+if answer.lower().strip() == "yes":
+    print("q")
+
     
 else:
     print("Oh well, farrell dear traverler")
