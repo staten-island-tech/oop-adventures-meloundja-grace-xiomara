eb8db21d3d4ca748a9dcf57b6cdfc1f21ba31e2c
Meloundja <124072293+Meloundja@users.noreply.github.com>
2023-05-09 10:58:05 -0400

    5/9

--- Intro.py
+++ Intro.py
@@ -5,12 +5,25 @@ if answer.lower().strip() == "yes":
     print("Whoopy")
     print("You have fifty days to make to the Kingdom, or your your treasure will forever be lost.")
     print("Within these fifty days, you'll be tasked with diffrent choices in which will either get you to your destination or you will unfourtnly be eliminated")
+
+class Traveler:
+    def __init__ (self, name, coins):
+        self.name = name 
+        self.coins = coins
+
+    def show(self):
+        print("Name is:", self.name, "and amount of coins is", self.coins)
+
+name = input("what is your name?")
+
+User = Traveler(name, 100)
+User.show()
+
    
 answer = input("are you ready for your first choice?")
 if answer.lower().strip() == "yes":
-    print("q")
+    print("")
 
     
 else:
-    print("Oh well, farrell dear traverler")
-    
+    print("Oh well, farrell dear traverler") 
