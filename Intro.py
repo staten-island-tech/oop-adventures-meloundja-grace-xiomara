60da53a2399a651e24d7975a56f835ba7a30624f
Grace-Lin130 <gracelin3884@gmail.com>
2023-05-09 11:02:19 -0400

    commit

--- Intro.py
+++ Intro.py
@@ -3,8 +3,8 @@ print ("Welcome dear traverler")
 answer = input("Would you like to embark on this journey? (yes/no)" )
 if answer.lower().strip() == "yes":
     print("Whoopy")
-    print("You have fifty days to make to the Kingdom, or your your treasure will forever be lost.")
-    print("Within these fifty days, you'll be tasked with diffrent choices in which will either get you to your destination or you will unfourtnly be eliminated")
+    print("You have thirty days to make to the Kingdom, or your your treasure will forever be lost.")
+    print("Within these thirty days, you'll be tasked with diffrent choices in which will either get you to your destination or you will unfourtnly be eliminated")
    
 answer = input("are you ready for your first choice?")
 if answer.lower().strip() == "yes":
---
+++ food decision.py
@@ -0,0 +1,9 @@
+option = input('what would you like to bring with yu on your travels?(meat/potion)')
+if option == 'meat':
+    print('cost is 90 gold coins')
+    print('do you still want to purchase this item?')
+
+elif option == 'potion':
+    print('cost is 50 gold coins')
+    print('do you still want to purchase this item? ')
+
