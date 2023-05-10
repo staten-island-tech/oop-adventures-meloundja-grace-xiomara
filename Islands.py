bd9a8c1be526c2f634f4a71dfaa99f589ec31723
XiomaraHernandezM <124072124+XiomaraHernandezM@users.noreply.github.com>
2023-05-10 11:00:36 -0400

    islnd

---
+++ Islands.py
@@ -0,0 +1,16 @@
+class Area():
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+    def __str__(self):
+        return f"{self.name}, {self.size}"
+
+class Island(Area):
+    def __init__(self, name, size, travel_time, description):
+        super().__init__(name, size)
+        self.travel_time = travel_time
+        self.description = description
+    def __str__(self):
+        return super().__str__()
+
+
