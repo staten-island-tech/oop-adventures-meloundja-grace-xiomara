87018484ea1915761bbc95a382f1de5c35a03402
Grace-Lin130 <gracelin3884@gmail.com>
2023-05-10 10:45:41 -0400

    intro commit

--- Intro.py
+++ Intro.py
@@ -4,13 +4,12 @@ answer = input("Would you like to embark on this journey? (yes/no)" )
 if answer.lower().strip() == "yes":
     print("Whoopy")
     print("You have thirty days to make to the Kingdom, or your your treasure will forever be lost.")
-    print("Within these thirty days, you'll be tasked with diffrent choices in which will either get you to your destination or you will unfourtnly be eliminated")
+    print("Within these thrity days, you'll be tasked with diffrent choices in which will either get you to your destination or you will unfourtnly be eliminated")
    
 answer = input("are you ready for your first choice?")
 if answer.lower().strip() == "yes":
-    print("q")
+    print("")
 
     
 else:
-    print("Oh well, farrell dear traverler")
-    
+    print("Oh well, farrell dear traverler") 
