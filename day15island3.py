1ccfa6c14e95f14279932308d285f1c9d5c2096a
Meloundja <124072293+Meloundja@users.noreply.github.com>
2023-05-18 10:58:19 -0400

    5/18

--- day15island3.py
+++ day15island3.py
@@ -1,9 +1,31 @@
 print("Congrats, you've made the correct choice and you're succsefully on your way to your beloved tresure")
+class Charater():
+    def __init__ (self, name, coins):
+        self.name = name 
+        self.coins = coins
 
-class Merchant():
-    def __init__ (self, suspicious):
-        self.suspicious = suspicious
     def __str__(self):
-        return f"{self.suspicious}"    
+        return f"{self.name}, {self.coins}"
+
+class Merchant(Charater):
+    def __init__ (self, name, coins, impression):
+        super().__init__(name, coins,)
+        self.name = name 
+        self.coins = coins  
+        self.impression = impression
+    def __str__(self):
+        return f"{self.name}, {self.coins}, {self.impression}"
     
-print(f"You've ran into a Merchant")
+def show(self):
+        print("You've ran into a", self.name, "and his amount of coins is", self.coins, "he comes off as", self.impression)
+
+
+name = "Old Merchant"
+impression = "suspicious"
+
+m = Merchant(name, 0, impression)
+
+
+
+
+    
