eca7c347e496c19e6cdfc76e1bca670a359f76bc
Grace-Lin130 <gracelin3884@gmail.com>
2023-05-18 11:02:06 -0400

    commit

--- Intro.py
+++ Intro.py
@@ -3,15 +3,17 @@ print ("Welcome dear traverler")
 answer = input("Would you like to embark on this journey? (yes/no)" )
 if answer.lower().strip() == "yes":
     print("Whoopy")
-    print("You have twenty five days to make to the Kingdom, or your your treasure will forever be lost.")
+    print("You have twenty five days to make to the Kingdom benevolent maðm, or your your treasure will forever be lost.")
     print("Within these twenty five days, you'll be tasked with diffrent choices in which will either get you to your destination or you will unfortunately be eliminated")
     print('remember to read each dialogue carefully, so you do not put in the wrong choice')
+elif answer.lower().strip() == "no":
+    print('goodbye')
    
 answer = input("are you ready for your first choice?")
 if answer.lower().strip() == "yes":
-    print("q")
+    print("Ok, you are in the land of the beginning, which is where your journey starts. Unfortunately, due to your terrible luck, you are broke. Your goal is to find the treasure hidden inside the Kingdom of benevolent maðm through a bunch of decisions and some might be deemed fatal.   ")
 
     
 else:
-    print("Oh well, farrell dear traverler")
+    print("Oh well, farewell dear traverler")
     
