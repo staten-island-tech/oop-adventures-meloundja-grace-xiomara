5fe8982d45d4eca98744a9df467476fd0668a3f9
XiomaraHernandezM <124072124+XiomaraHernandezM@users.noreply.github.com>
2023-05-26 10:59:33 -0400

    game combin

--- rage the treasure.py
+++ rage the treasure.py
@@ -9,34 +9,42 @@ time.sleep(t)
 #introduction
 print ("Welcome dear traveler")
 
+def journey():
+    answer = input("Would you like to embark on this journey? (yes/no)" )
+    if answer.lower().strip() == "yes":
+        print("Whoopy")
+        time.sleep(t)
+        print("You have twenty five days to make to the Kingdom benevolent falls, or your your treasure will forever be lost.")
+        time.sleep(t2)
+        print("Within these twenty five days, you'll be tasked with different choices in which will either get you to your destination or you will unfortunately be eliminated")
+        time.sleep(t2)
+        print('remember to read each dialogue carefully, so you do not put in the wrong choice')
+        first_choice()
+    else:
+        x = True
+        while x == True:
+            q = input("Would you like to continue? Y/N: ")
+            if (q=="No" or q=="N" or q=="n" or q=="no"):
+                x = False
+                break
+journey()
 
-answer = input("Would you like to embark on this journey? (yes/no)" )
-if answer.lower().strip() == "yes":
-    print("Whoopy")
-    time.sleep(t)
-    print("You have twenty five days to make to the Kingdom benevolent falls, or your your treasure will forever be lost.")
-    time.sleep(t2)
-    print("Within these twenty five days, you'll be tasked with different choices in which will either get you to your destination or you will unfortunately be eliminated")
-    time.sleep(t2)
-    print('remember to read each dialogue carefully, so you do not put in the wrong choice')
-elif answer.lower().strip() == "no":
-    print('goodbye')
+def first_choice():  
+    answer2 = input("are you ready for your first choice?")
+    if answer2.lower().strip() == "yes":
+        print("Ok, you are in the land of the beginning, which is where your journey starts. Unfortunately, due to your terrible luck, you are broke. Your goal is to find the treasure hidden inside the Kingdom of benevolent falls through a bunch of decisions and some might be deemed fatal.   ")
+        food_optn1()
+    else:
+        x = True
+        while x == True:
+            q = input("Would you like to continue? Y/N: ")
+            if (q=="No" or q=="N" or q=="n" or q=="no"):
+                x = False
+                break
 
 
 
-   
-answer2 = input("are you ready for your first choice?")
-if answer2.lower().strip() == "yes":
-    print("Ok, you are in the land of the beginning, which is where your journey starts. Unfortunately, due to your terrible luck, you are broke. Your goal is to find the treasure hidden inside the Kingdom of benevolent falls through a bunch of decisions and some might be deemed fatal.   ")
-
-
-   
-else:
-    print("Oh well, farewell dear traveler")
-
-
-
-   
+    
 
     #AD SOETHING HERE TATH CONECTS TO THE EXT DAY 
 
@@ -46,64 +54,66 @@ else:
 #food decision/day 5
 
 
-
-option = input('what would you like to bring with you on your travels? Meat is 90 gold coins and potion is 50 gold coins. They will both last 50 days of your adventure(meat/potion)')
-if option == 'meat':
-    print('cost is 90 gold coins')
-    choice = input('do you still want to purchase this item?')
-    if choice == 'yes':
-        print('you have 10 coins left')
-    elif choice == 'no':
-        print('Okay so you want potion instead? (yes1/no)')
-        choice = input()
-        if choice == 'yes1':
-            print('okay you have 50 coins left and continued on your journey')
+def food_optn1():
+    option = input('what would you like to bring with you on your travels? Meat is 90 gold coins and potion is 50 gold coins. They will both last 50 days of your adventure(meat/potion)')
+    if option == 'meat':
+        print('cost is 90 gold coins')
+        choice = input('do you still want to purchase this item?')
+        if choice == 'yes':
+            print('you have 10 coins left')
         elif choice == 'no':
-            print('thank you for visiting but unfortunately you will die due to starvation on your adventure')
+            print('Okay so you want potion instead? (yes1/no)')
             choice = input()
-           
-        else:
-            print('not a choice')
+            if choice == 'yes1':
+                print('okay you have 50 coins left and continued on your journey')
+            elif choice == 'no':
+                print('thank you for visiting but unfortunately you will die due to starvation on your adventure')
+                choice = input()
+            
+            else:
+                print('not a choice')
+                
 
 
-elif option == 'potion':
-    print('cost is 50 gold coins')
-    choice = input('do you still want to purchase this item?')
-    if choice == 'yes':
-        print('you have 50 coins left')
-    elif choice == 'no':
-        print('Okay do you want meat instead? (yes2/no)')
-        choice = input()
-        if choice == 'yes2':
-            print('okay you have 10 coins left and continued on your journey ')
+    elif option == 'potion':
+        print('cost is 50 gold coins')
+        choice = input('do you still want to purchase this item?')
+        if choice == 'yes':
+            print('you have 50 coins left')
         elif choice == 'no':
-            print('thank you for visiting but unfortunately you will die due to starvation on your adventure')
+            print('Okay do you want meat instead? (yes2/no)')
             choice = input()
+            if choice == 'yes2':
+                print('okay you have 10 coins left and continued on your journey ')
+            elif choice == 'no':
+                print('thank you for visiting but unfortunately you will die due to starvation on your adventure')
+                choice = input()
 
 
-else:
-    print('not in stock')
-next_option = input()
+    else:
+        print('not in stock')
+    next_option = input()
 
 
-if next_option == 'meat' or 'yes2':
-    print('This is day 5 and it is time to make a new decision. Do you want to eat the food you bought? If you do not eat now then you will be eliminated. type yes to eat. Type no to die')
-    choice = input()
-    if choice == 'yes':
-        print('ok, you may continue on your adventure')
-    elif choice == 'no':
-        print('You have starved to death and died. YOUR JOURNEY ENDS')
+    if next_option == 'meat' or 'yes2':
+        print('This is day 5 and it is time to make a new decision. Do you want to eat the food you bought? If you do not eat now then you will be eliminated. type yes to eat. Type no to die')
+        choice = input()
+        if choice == 'yes':
+            print('ok, you may continue on your adventure')
+            islnchoice_s()
+        elif choice == 'no':
+            print('You have starved to death and died. YOUR JOURNEY ENDS')
 
 
 
 
-elif next_option == 'potion' or 'yes1':
-    print('This is day 5 and it is time to make a new decision. Do you want to use the potion you bought? If you do not eat now then you will be eliminated. type yes to eat. Type no to die')
-    choice = input()
-    if choice == 'yes':
-        print('Oops, seems like the potion you bought is poisoned so you just died ')
-    elif choice == 'no':
-        print('You have starved to death and died. YOUR JOURNEY ENDS')
+    elif next_option == 'potion' or 'yes1':
+        print('This is day 5 and it is time to make a new decision. Do you want to use the potion you bought? If you do not eat now then you will be eliminated. type yes to eat. Type no to die')
+        choice = input()
+        if choice == 'yes':
+            print('Oops, seems like the potion you bought is poisoned so you just died ')
+        elif choice == 'no':
+            print('You have starved to death and died. YOUR JOURNEY ENDS')
 
 #Day 10
 class Area():
@@ -128,154 +138,154 @@ island3 = Island('Lost Souls Springs', 'Small Island', '20 Days to Travel', '')
 
 
 
+def islnchoice_s():
+    print("You are presented with these two islands to travel to. Would you like to see the information?")
+    island_pick = input()
+    if island_pick == 'yes':
+        print(island2)
+        print(island3)
+        choice = input("Are you ready to choose now?")
+        if choice == 'yes':
+            print('Alright. Make your choice now....')
+            print("Type anything to continue")
+            next_choice = input()
+        elif choice == 'no':
+            print('Alright, take some time to decide')
+            print("Type anything to continue")
+            next_choice = input()
+        else:
+            print('error')
 
-print("You are presented with these two islands to travel to. Would you like to see the information?")
-island_pick = input()
-if island_pick == 'yes':
-    print(island2)
-    print(island3)
-    choice = input("Are you ready to choose now?")
-    if choice == 'yes':
-        print('Alright. Make your choice now....')
-        print("Type anything to continue")
-        next_choice = input()
-    elif choice == 'no':
-        print('Alright, take some time to decide')
-        print("Type anything to continue")
-        next_choice = input()
+
+
+
+    elif island_pick == 'no':
+        print("Are you sure you do not want to see the descriptions for these islands?")
+        print("This is your last chance to make a decision")
+        choose = input()
+        if choose == 'yes':
+            print('Alright. Please, make a choice now...')
+            print("Type anything to continue")
+            next_choice = input()
+        elif choose == 'no':
+            print("So, would you like to see the island descriptions")
+            choose2 = input()
+            if choose2 == 'yes':
+                print(island2)
+                print(island3)
+                print('Make your choice now, please. Your decision awaits.')
+                print("Type anything to continue")
+                next_choice = input()
+            elif choose2 == 'no':
+                print("....")
+                print("....")
+                print("....")
+                print("....")
+                print("....")
+                print("....")
+                print("....")
+                print("That was your last chance to see the island options. Make a choice now if you will")
+                print("Type anything to continue")
+                next_choice = input()
+
+
+
     else:
         print('error')
 
 
 
 
-elif island_pick == 'no':
-    print("Are you sure you do not want to see the descriptions for these islands?")
-    print("This is your last chance to make a decision")
-    choose = input()
-    if choose == 'yes':
-        print('Alright. Please, make a choice now...')
-        print("Type anything to continue")
-        next_choice = input()
-    elif choose == 'no':
-        print("So, would you like to see the island descriptions")
-        choose2 = input()
-        if choose2 == 'yes':
-            print(island2)
-            print(island3)
-            print('Make your choice now, please. Your decision awaits.')
-            print("Type anything to continue")
-            next_choice = input()
-        elif choose2 == 'no':
-            print("....")
-            print("....")
-            print("....")
-            print("....")
-            print("....")
-            print("....")
-            print("....")
-            print("That was your last chance to see the island options. Make a choice now if you will")
-            print("Type anything to continue")
-            next_choice = input()
+    for i in next_choice:
+        print()
+    
 
 
-
-else:
-    print('error')
-
-
-
-
-for x in next_choice:
-    print()
- 
+    print("Alright, your decision awaits.")
+    print("Please choose between Island 2 or Island 3. Type '2' for Island 2 and '3' for Island 3")
+    island_choice_omg = input()
+    if island_choice_omg == '2':
+        print("You have chosen Island 2")
 
 
-print("Alright, your decision awaits.")
-print("Please choose between Island 2 or Island 3. Type '2' for Island 2 and '3' for Island 3")
-island_choice_omg = input()
-if island_choice_omg == '2':
-    print("You have chosen Island 2")
+    elif island_choice_omg == '3':
+        print("You have chosen Island 3")
 
 
-elif island_choice_omg == '3':
-    print("You have chosen Island 3")
-
-
-else:
-    print('error')
+    else:
+        print('error')
 
 #Day15
-
-print("congrats dear traveler, you've made it to day 15 and you have cosen island 2!")
-print("an important choice awaits, choose wisly...")
-print("you must choose between giving up ownership of either you gold coins or your items")
-
-
-choice = input("what are you willing to give up? gold coins or items?")
+def day15fnl():
+    print("congrats dear traveler, you've made it to day 15 and you have cosen island 2!")
+    print("an important choice awaits, choose wisly...")
+    print("you must choose between giving up ownership of either you gold coins or your items")
 
 
-if choice == "gold coins":
-    print("unfortunately  this island is a death sentence, lack of coins or items guarantee death")
+    choice = input("what are you willing to give up? gold coins or items?")
 
 
-elif choice == "items":
-    print("unfortunately  this island is a death sentence, lack of items or coins guarantee death")
+    if choice == "gold coins":
+        print("unfortunately  this island is a death sentence, lack of coins or items guarantee death")
 
 
-print("game over")
+    elif choice == "items":
+        print("unfortunately  this island is a death sentence, lack of items or coins guarantee death")
 
 
-from time import sleep
-print("Congrats, you've made the correct choice and you're successfully on your way to your beloved treasure")
-class Character():
-    def __init__ (self, name, coins):
-        self.name = name
-        self.coins = coins
+    print("game over")
 
 
-    def __str__(self):
-        return f"{self.name}, {self.coins}"
-x = 2
-class Merchant(Character):
-    def __init__ (self, name, coins, impression):
-        super().__init__(name, coins,)
-        self.name = name
-        self.coins = coins  
-        self.impression = impression
- 
-        print("You've ran into a",self.name,"and his amount of coins is", self.coins, "he comes off as", self.impression)
-        sleep(x)
+    from time import sleep
+    print("Congrats, you've made the correct choice and you're successfully on your way to your beloved treasure")
+    class Character():
+        def __init__ (self, name, coins):
+            self.name = name
+            self.coins = coins
+
+
+        def __str__(self):
+            return f"{self.name}, {self.coins}"
+    x = 2
+    class Merchant(Character):
+        def __init__ (self, name, coins, impression):
+            super().__init__(name, coins,)
+            self.name = name
+            self.coins = coins  
+            self.impression = impression
+    
+            print("You've ran into a",self.name,"and his amount of coins is", self.coins, "he comes off as", self.impression)
+            sleep(x)
 
 
 
 
-name = "Old Merchant"
-impression = "suspicious"
+    name = "Old Merchant"
+    impression = "suspicious"
 
 
-m = Merchant(name, 0, impression)
+    m = Merchant(name, 0, impression)
 
 
-print("The merchant promises to gain you access to the island in four days, do you believe him?")
-sleep(x)
+    print("The merchant promises to gain you access to the island in four days, do you believe him?")
+    sleep(x)
 
 
-choice = input("Do you agree?")
-sleep(1)
-
-
-if choice == "yes" or "Y" or "y":
-    print("you've made a horrible choice, you've been tricked and now you'll die")
+    choice = input("Do you agree?")
     sleep(1)
 
 
-elif choice == "no" or "N" or "n":
-    print("Congrats, he is a horrible trickster and you've just avoided death!")
+    if choice == "yes" or "Y" or "y":
+        print("you've made a horrible choice, you've been tricked and now you'll die")
+        sleep(1)
 
 
-else:
-    print("error")
+    elif choice == "no" or "N" or "n":
+        print("Congrats, he is a horrible trickster and you've just avoided death!")
+
+
+    else:
+        print("error")
 
 #Day 20
 
---
+++ test.py
@@ -0,0 +1,76 @@
+#Day 5 (test)
+
+
+option = input('what would you like to bring with you on your travels? Meat is 90 gold coins and potion is 50 gold coins. They will both last 50 days of your adventure(meat/potion)')
+if option == 'meat':
+    print('cost is 90 gold coins')
+    choice = input('do you still want to purchase this item?')
+    if choice == 'yes':
+        print('you have 10 coins left')
+        next_thing = input()
+
+    elif choice == 'no':
+        print('Okay so you want potion instead? (yes1/no)')
+        choice = input()
+        if choice == 'yes1':
+            print('okay you have 50 coins left and continued on your journey')
+            next_thing = input()
+        elif choice == 'no':
+            print('thank you for visiting but unfortunately you will die due to starvation on your adventure')
+        else:
+            print('error')
+
+    else:
+        print('error')
+
+
+elif option == 'potion':
+    print('cost is 50 gold coins')
+    choice = input('do you still want to purchase this item?')
+    if choice == 'yes':
+        print('you have 50 coins left')
+    elif choice == 'no':
+        print('Okay do you want meat instead? (yes2/no)')
+        choice = input()
+        if choice == 'yes2':
+            print('okay you have 10 coins left and continued on your journey ')
+        elif choice == 'no':
+            print('thank you for visiting but unfortunately you will die due to starvation on your adventure')
+            choice = input()
+
+        else:
+            print('error')
+    else:
+        print('error')
+
+
+else:
+    print('not in stock')
+
+
+
+
+
+
+for i in next_thing:
+    print()
+
+    print('This is day 5 and it is time to make a new decision. Do you want to eat the meat you bought? If you do not eat now then you will be eliminated. type yes to eat. Type no to die')
+    choice = input()
+    if choice == 'yes':
+        print('ok, you may continue on your adventure')
+    elif choice == 'no':
+        print('You have starved to death and died. YOUR JOURNEY ENDS')
+
+
+
+
+elif next_option == 'potion' or 'yes1':
+    print('This is day 5 and it is time to make a new decision. Do you want to use the potion you bought? If you do not eat now then you will be eliminated. type yes to eat. Type no to die')
+    choice = input()
+    if choice == 'yes':
+        print('Oops, seems like the potion you bought is poisoned so you just died ')
+    elif choice == 'no':
+        print('You have starved to death and died. YOUR JOURNEY ENDS')
+
+
