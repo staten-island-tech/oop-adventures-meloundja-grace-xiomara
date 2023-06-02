04ff9dfe4b4c7d80f16e3cc7461ef50f43d74d90
Meloundja <124072293+Meloundja@users.noreply.github.com>
2023-06-02 11:22:11 -0400

    6/2

--- Intro.py
+++ Intro.py
@@ -1,15 +1,43 @@
+from time import sleep 
+x =1.5
 print ("Welcome dear traverler")
+sleep(x)
 
 answer = input("Would you like to embark on this journey? (yes/no)" )
 if answer.lower().strip() == "yes":
+    sleep(x)
     print("Whoopy")
-    print("You have thirty days to make to the Kingdom, or your your treasure will forever be lost.")
-    print("Within these thrity days, you'll be tasked with diffrent choices in which will either get you to your destination or you will unfourtnly be eliminated")
-   
-answer = input("are you ready for your first choice?")
-if answer.lower().strip() == "yes":
-    print("")
+    sleep(x)
+    print("You have fifty days to make to the Kingdom, or your your treasure will forever be lost.")
+    sleep(x)
+    print("Within these fifty days, you'll be tasked with diffrent choices in which will either get you to your destination or you will unfourtnly be eliminated")
+    sleep(x)
 
-    
 else:
     print("Oh well, farrell dear traverler") 
+    exit()
+
+class Charater():
+    def __init__ (self, name, coins):
+        self.name = name 
+        self.coins = coins
+
+    def show(self):
+        print("Your name is", self.name, "and amount of coins is", self.coins)
+
+name = input("what is your name?")
+sleep(x)
+User = Charater(name, 100)
+User.show()
+
+   
+answer = input(f"are you ready for your first choice {name}")
+if answer.lower().strip() == "yes":
+    sleep(x)
+    print("Great!")
+else:
+    print("too bad, you're still choosing.")
+
+
+
+
